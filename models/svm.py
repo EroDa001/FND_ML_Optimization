89c74b4de420dea3078551565361d6d3e23ec011
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-04-30 04:56:43 +0100

    Add SVM Model

---
+++ models/svm.py
@@ -0,0 +1,34 @@
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+def create_model(params):
+    model = SVC(
+        C=params["C"],
+        gamma=params["gamma"],
+        kernel=params["kernel"],
+        probability=True,
+        random_state=1337,
+    )
+    return model
+
+
+def default_params():
+    return {"C": 1.0, "gamma": "scale", "kernel": "rbf"}
+
+
+def param_space():
+    return [
+        {"name": "C", "type": "continuous", "bounds": [0.1, 10]},
+        {"name": "gamma", "type": "continuous", "bounds": [0.0001, 1]},
+        {
+            "name": "kernel",
+            "type": "categorical",
+            "categories": ["linear", "rbf", "poly"],
+        },
+    ]
+
+
+def evaluate(model, X_val, y_val):
+    y_pred = model.predict(X_val)
+    return accuracy_score(y_val, y_pred)
