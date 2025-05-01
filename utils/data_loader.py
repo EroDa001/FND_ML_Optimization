c6423b777c1ca36a4bc7da5bf947c6971b897ee2
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-05-01 17:15:51 +0100

    Fix to use config file instead

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,6 +5,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+from experiments.config import RANDOM_SEED
+
 
 def load_data():
     path = kagglehub.dataset_download(
@@ -51,7 +53,7 @@ def load_data():
     X_scaled = scaler.fit_transform(X)
 
     X_train, X_val, y_train, y_val = train_test_split(
-        X_scaled, y, test_size=0.2, random_state=1337, stratify=y
+        X_scaled, y, test_size=0.2, random_state=RANDOM_SEED, stratify=y
     )
 
-    return X_train, X_val, y_train, y_val
+    return X_train, y_train, X_val, y_val
