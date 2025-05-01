db41e84934a6eeeba69295707414acdb3ac029a6
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-05-01 05:37:59 +0100

    Add more metrics to track

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,7 @@
-from sklearn.metrics import (accuracy_score, classification_report,
-                             cohen_kappa_score, f1_score, precision_score,
-                             recall_score, roc_auc_score)
+from sklearn.metrics import (accuracy_score, balanced_accuracy_score,
+                             classification_report, cohen_kappa_score,
+                             f1_score, log_loss, matthews_corrcoef,
+                             precision_score, recall_score, roc_auc_score)
 
 
 def compute_metrics(y_true, y_pred, y_prob=None, average="macro"):
@@ -10,10 +11,13 @@ def compute_metrics(y_true, y_pred, y_prob=None, average="macro"):
         "Recall": recall_score(y_true, y_pred, average=average, zero_division=0),
         "F1": f1_score(y_true, y_pred, average=average, zero_division=0),
         "CohenKappa": cohen_kappa_score(y_true, y_pred),
+        "MatthewsCorrCoef": matthews_corrcoef(y_true, y_pred),
+        "BalancedAccuracy": balanced_accuracy_score(y_true, y_pred),
     }
     if y_prob is not None:
         # for binary: pass y_prob[:,1]; for multiclass: full matrix
         metrics["ROC_AUC"] = roc_auc_score(y_true, y_prob[:, 1], multi_class="ovr")
+        metrics["LogLoss"] = log_loss(y_true, y_prob)
     return metrics
 
 
